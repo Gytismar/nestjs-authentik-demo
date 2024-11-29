5174379882004763f31d6f682b4d1f287f1dca3f
winqus <67459222+winqus@users.noreply.github.com>
2024-11-29 20:02:42 +0200

    Remove unused import

--- authentik-angular-demo/src/app/app.config.ts
+++ authentik-angular-demo/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptorFn } from '../auth/auth.interceptor';
-import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authRoute } from '../auth/auth.routes';
